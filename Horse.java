420f15db72ec729242fb237b6edea99f9fa6a127
Kayla Ribeiro <77889723+dazy821@users.noreply.github.com>
2023-02-26 20:10:11 +0000

    Completed the assignment

--- Horse.java
+++ Horse.java
@@ -1,1 +1,16 @@
 //Create the Horse class here.
+
+public class Horse{
+
+  String name;
+  int weight;
+  boolean tame;
+
+    public Horse(String name, int weight, boolean tame){
+
+      this.name= name;
+      this.weight= weight;
+      this.tame= tame;
+    }
+  
+}
--- Stable.java
+++ Stable.java
@@ -1,1 +1,28 @@
 //Create the Stable class here.
+import java.util.ArrayList;
+
+public class Stable{
+
+  String address;
+  char decision;
+  ArrayList<Horse> al= new ArrayList<Horse>();
+
+    public Stable(String address, char decision){
+
+      this.address= address;
+      this.decision= decision;
+    }
+  public Stable(){}
+
+  public void show(){
+    for(int i =0 ; i < al.size(); i++){
+
+   System.out.println("Horse #" + (i+1) + " Name: " +al.get(i).name+ " Weight: " + al.get(i).weight + " Tame: " +al.get(i).tame);
+
+      
+    }
+ 
+    
+  }
+  
+}
